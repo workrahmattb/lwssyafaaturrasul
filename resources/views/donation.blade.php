de2ffa372dbf4085df2bf6490d05a497562b58eb
workrahmattb <workrahmattb@gmail.com>
2026-04-01 07:25:25 +0700

    logo ditab nya pake logo lws

--- resources/views/donation.blade.php
+++ resources/views/donation.blade.php
@@ -14,6 +14,10 @@
 
     <title>Lembaga Wakaf Syafa'aturrasul - Salurkan Kebaikan</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('images/lws.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('images/lws.png') }}">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
